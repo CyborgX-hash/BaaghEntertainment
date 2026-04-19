ce4d23e87931fddaed624a4148e142a30005bd86
CyborgX-hash <sakshamsontakke713@gmail.com>
2026-04-19 13:27:31 +0530

    Configure Neon Serverless Driver for Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from '@prisma/client';
-import { Pool } from '@neondatabase/serverless';
+import { Pool, neonConfig } from '@neondatabase/serverless';
 import { PrismaNeon } from '@prisma/adapter-neon';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 function createPrismaClient() {
-    return new PrismaClient({ log: ['query'] });
+    if (!process.env.DATABASE_URL) {
+        console.warn("DATABASE_URL is not defined in the environment.");
+    }
+    
+    // Create a serverless Neon connection pool
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaNeon(pool);
+    
+    // Create the Prisma Client using the driver adapter
+    return new PrismaClient({ adapter, log: ['error', 'warn'] });
 }
 
 export const prisma = globalForPrisma.prisma || createPrismaClient();
